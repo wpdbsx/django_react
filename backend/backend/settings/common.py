e08d66df54285d280b22be2877b9a6826f57ea9f
wpdbsx <wpdbsx@naver.com>
2020-11-26 18:21:09 +0900

    back

--- backend/backend/settings/common.py
+++ backend/backend/settings/common.py
@@ -32,12 +32,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django appss
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third apps
+    # "psycopg2"
+    # local apps
+    "instagram",
+    "account",
 ]
 
 MIDDLEWARE = [
@@ -74,14 +80,24 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "django_react",  # DB name
+#         "USER": "root",  # DB account
+#         "PASSWORD": "1234",  # DB account's password
+#         "HOST": "127.0.0.1",  # DB address(IP)
+#         "PORT": "3306",  # DB port(normally 3306)
+#     }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "django_react",  # DB name
-        "USER": "root",  # DB account
-        "PASSWORD": "1234",  # DB account's password
-        "HOST": "127.0.0.1",  # DB address(IP)
-        "PORT": "3306",  # DB port(normally 3306)
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "django_react",
+        "USER": "wpdbsx",
+        "PASSWORD": "1234",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -97,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
 ]
-
+AUTH_USER_MODEL = "account.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
